54653c7141c409b15b90651d5da126b990ebbabc
gege <2575743494@qq.com>
2023-12-25 08:15:36 +0800

    新建更改列表

--- 邱珍萍/促销调价和促销赠送存储过程.sql
+++ 邱珍萍/促销调价和促销赠送存储过程.sql
@@ -1,5 +1,9 @@
 --促销调价单修改业务机构,1是新增,2是删除,删除时第一个业务机构删不了
-call proc_update_cxtjd('202308090000002','83602',1);
+call proc_update_cxtjd('202202240000009','83315',1);
 
 --促销赠送单修改业务机构,1是新增,2是删除,删除时第一个业务机构删不了
-call proc_update_cxzsd('2301020000089','83662',1);
+call proc_update_cxzsd('2301020000083','83315',1);
+call proc_update_cxzsd('2301020000084','83315',1);
+call proc_update_cxzsd('2301020000086','83315',1);
+call proc_update_cxzsd('2301020000085','83315',1);
+call proc_update_cxzsd('2301020000087','83315',1);
