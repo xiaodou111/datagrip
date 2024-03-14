c18766e07e1852b5e7d3a1ac6efa0c0087af49a3
gege <2575743494@qq.com>
2024-03-14 11:00:00 +0800

    每天更新医保商品信息结算明细表存储过程+重新循环医保结算明细基础表数据

--- 李玲/重新循环医保结算明细基础表数据.sql
+++ 李玲/重新循环医保结算明细基础表数据.sql
@@ -1,14 +1,15 @@
- DECLARE
-    P_StartDate DATE :=DATE'2023-01-01';  -- number of rows to insert in each batch
-   P_EndDate DATE :=DATE '2024-03-12';
-   BatchSize NUMBER := 100000;
-   P_CurrentDate DATE :=P_StartDate;
-   v_count NUMBER;   -- offset for each batch
+--todo 循环医保明细
+DECLARE
+    P_StartDate DATE := DATE '2023-01-01';  -- 起始日期
+    P_EndDate DATE := DATE '2024-04-01';  -- 结束日期
+    BatchSize NUMBER := 100000;  -- 每个批次的大小
+    P_CurrentDate DATE := TRUNC(P_StartDate, 'MM');  -- 将当前日期设为当前月的第一天
+    v_count NUMBER;  -- 偏移量，用于批处理
 BEGIN
    WHILE P_CurrentDate <= P_EndDate
     LOOP
- insert into d_ybzd_test
-with a1 as (select d.BUSNO, tb.CLASSNAME, d.SALENO, d.WAREID, d.WAREQTY, d.NETAMT, d.NETPRICE, ext.WE_SCHAR01,
+ insert into D_YBZD_detail
+with a1 as (select yh.RECEIPTDATE,d.BUSNO, tb.CLASSNAME, d.SALENO,d.SALER, d.WAREID, d.WAREQTY, d.NETAMT, d.NETPRICE, ext.WE_SCHAR01,
 --        ext.WE_NUM04 as 药店医保支付价,
 --        ext.WE_NUM05 as 门诊医保支付价,
                    decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05) as 门店诊所医保支付价,
@@ -41,19 +42,20 @@ with a1 as (select d.BUSNO, tb.CLASSNAME, d.SALENO, d.WAREID, d.WAREQTY, d.NETAM
             from t_sale_d d
                      join D_ZHYB_HZ_CYB cyb on d.SALENO = cyb.ERP销售单号
                      join t_yby_order_h yh on yh.ERPSALENO = cyb.ERP销售单号
-                     join t_yby_order_d_temp yd on yh.ORDERNO=yd.ORDERNO and yd.WARECODE=d.WAREID
+                     join (select ORDERNO,WARECODE,EXT_CHAR08 from T_YBY_ORDER_D_TEMP
+group by  ORDERNO,WARECODE, EXT_CHAR08) yd  on yh.ORDERNO=yd.ORDERNO and yd.WARECODE=d.WAREID
                      join t_ware_ext ext on d.WAREID = ext.WAREID and ext.COMPID = 1000
                      join t_busno_class_set ts on d.busno = ts.busno and ts.classgroupno = '305'
                      join t_busno_class_base tb on ts.classgroupno = tb.classgroupno and ts.classcode = tb.classcode
                 --and d.SALENO in ('2401304551007053', '2401271247060521')
 --           and cyb.BUSNO in (81499,81501,84576)
-        and  cyb.销售日期 between P_CurrentDate and P_CurrentDate+1
+        and  cyb.销售日期 between P_CurrentDate and ADD_MONTHS(P_CurrentDate, 1)
 --                 and cyb.销售日期>=date'2023-01-01'
         and cyb.异地标志='非异地'
 --   TO_DATE('20240101 07:10:00', 'YYYYMMDD HH24:MI:SS')
 --     AND TO_DATE('20240104 07:10:00', 'YYYYMMDD HH24:MI:SS')
             ),
-     a2 as (select BUSNO, CLASSNAME, SALENO, WAREID,MAKENO,INVALIDATE, WAREQTY, NETAMT, NETPRICE, WE_SCHAR01, 门店诊所医保支付价,
+     a2 as (select RECEIPTDATE,BUSNO, CLASSNAME, SALENO,SALER, WAREID,MAKENO,INVALIDATE, WAREQTY, NETAMT, NETPRICE, WE_SCHAR01, 门店诊所医保支付价,
                    医保超限价, EXT_CHAR08,
                    case
                        when WE_SCHAR01 = '医保乙' then round(EXT_CHAR08 * (a1.NETAMT - 医保超限价), 2)
@@ -80,7 +82,7 @@ with a1 as (select d.BUSNO, tb.CLASSNAME, d.SALENO, d.WAREID, d.WAREQTY, d.NETAM
                                 else round(a2.单据明细医保支付价 / b.整单医保支付价, 4) end   as 单据明细医保比例
                from a2
                 join yb_zfze b on a2.SALENO = b.SALENO)
-select  a2.BUSNO, a2.CLASSNAME, a2.SALENO, a2.WAREID,a2.WAREQTY,a2.MAKENO,a2.INVALIDATE, a2.NETAMT as 实价金额, a2.NETPRICE as 实价,
+select a2.RECEIPTDATE,a2.BUSNO, a2.CLASSNAME, a2.SALENO,a2.SALER, a2.WAREID,a2.WAREQTY,a2.MAKENO,a2.INVALIDATE, a2.NETAMT as 实价金额, a2.NETPRICE as 实价,
        a2.WE_SCHAR01,
        a2.医疗费用自费总额,
        单据明细医保比例,
@@ -103,7 +105,7 @@ from a2
          left join yb_bl bl on a2.SALENO = bl.SALENO and a2.WAREID = bl.WAREID  and a2.WAREQTY=bl.WAREQTY;
   COMMIT;
 
-  P_CurrentDate := P_CurrentDate + 1;
+  P_CurrentDate := ADD_MONTHS(P_CurrentDate, 1);
      DBMS_LOCK.SLEEP(1); -- increase offset for next batch
         -- commit changes after each batch
    END LOOP;
