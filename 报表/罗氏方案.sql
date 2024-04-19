aeebe7c477e04384457cd04666b79871c0044175
gege <2575743494@qq.com>
2024-04-19 21:26:17 +0800

    罗氏修改

--- 报表/罗氏方案.sql
+++ 报表/罗氏方案.sql
@@ -6,9 +6,9 @@ select a.busno, s.ORGNAME, a.idcardno, a.username, a.programme, a.wareid, w.WARE
 from d_luoshi_prog a
          left join s_busi s on a.busno = s.BUSNO
          left join t_ware_base w on a.wareid = w.WAREID
-单靶(单曲妥珠单抗)~t1/双靶(帕妥珠单抗)~t2/双靶(曲妥珠单抗)~t3/其他方案(含曲妥珠单抗)~t4/其他方案(含帕妥珠单抗)~t5/单靶(曲妥珠单抗HSC)~t6/双靶(含曲妥珠单抗HSC)~t7/双靶(帕妥珠单抗)~t8/其他方案(曲妥珠单抗HSC+吡咯替尼)~t9
+-- 单靶(单曲妥珠单抗)~t1/双靶(帕妥珠单抗)~t2/双靶(曲妥珠单抗)~t3/其他方案(含曲妥珠单抗)~t4/其他方案(含帕妥珠单抗)~t5/单靶(曲妥珠单抗HSC)~t6/双靶(含曲妥珠单抗HSC)~t7/双靶(帕妥珠单抗)~t8/其他方案(曲妥珠单抗HSC+吡咯替尼)~t9/~t /
 
-     alter table D_LUOSHI_PROG
+alter table D_LUOSHI_PROG
     add enddate date;
 --插入时不指定ENDDATE字段会使用默认值
 insert into D_LUOSHI_PROG(BUSNO, IDCARDNO, USERNAME, PROGRAMME, WAREID, BEGINDATE)
@@ -16,19 +16,20 @@ values (null, 332623195005301346, '里斯', 9, 10502445, DATE'2023-12-31');
 ALTER TABLE D_LUOSHI_PROG
     MODIFY (enddate DATE DEFAULT TO_DATE('9999-12-31', 'YYYY-MM-DD'));
 
--- select h.IDCARDNO,a.busno,cyb.参保地,cyb.异地标志,h.USERNAME,h.CAGE,h.SEX,d.WAREID,d.WAREQTY,a.ACCDATE
---                     from t_remote_prescription_h h
---                         join t_sale_h a on substr(a.notes, 0,
---                                                   decode(instr(a.notes, ' '), 0, length(a.notes) + 1,
---                                                          instr(a.notes, ' ')) -
---                                                   1) =
---                                            h.CFNO
---                         join t_sale_d d on a.SALENO = d.SALENO
---       left join D_ZHYB_HZ_CYB cyb on cyb.ERP销售单号 = a.SALENO
---       where
---           IDCARDNO='332623195005301346' AND
---           a.ACCDATE between date'2023-01-01' and date'2023-09-01' and
--- d.WAREID in (10502445);
+select h.IDCARDNO,a.busno,cyb.参保地,cyb.异地标志,h.USERNAME,h.CAGE,h.SEX,d.WAREID,d.WAREQTY,a.ACCDATE
+                    from t_remote_prescription_h h
+                        join t_sale_h a on substr(a.notes, 0,
+                                                  decode(instr(a.notes, ' '), 0, length(a.notes) + 1,
+                                                         instr(a.notes, ' ')) -
+                                                  1) =
+                                           h.CFNO
+                        join t_sale_d d on a.SALENO = d.SALENO
+      left join D_ZHYB_HZ_CYB cyb on cyb.ERP销售单号 = a.SALENO
+      where
+          IDCARDNO='332623195005301346' AND
+          a.ACCDATE between date'2023-01-01' and date'2023-09-01' and
+d.WAREID in (10502445);
+
 --方案一
 select d.WAREID, h.IDCARDNO, a.ACCDATE, d.WAREQTY
 from t_remote_prescription_h h
@@ -36,29 +37,15 @@ from t_remote_prescription_h h
               on SUBSTR(a.notes, 0, DECODE(INSTR(a.notes, ' '), 0, LENGTH(a.notes) + 1, INSTR(a.notes, ' ')) - 1) =
                  h.CFNO
          join t_sale_d d on a.SALENO = d.SALENO
-         join (SELECT p.IDCARDNO, p.PROGRAMME, p.BEGINDATE,
-                      LEAD(p.BEGINDATE, 1) OVER (PARTITION BY p.IDCARDNO ORDER BY p.BEGINDATE) AS ENDDATE
-FROM D_LUOSHI_PROG p
-WHERE p.PROGRAMME = 1) prog ON h.IDCARDNO = prog.IDCARDNO AND
-                               (a.ACCDATE BETWEEN prog.BEGINDATE AND prog.ENDDATE OR a.ACCDATE > prog.BEGINDATE)
-WHERE d.WAREID IN (10502445);
-
-select d.WAREID, h.IDCARDNO, a.ACCDATE, d.WAREQTY
-from t_remote_prescription_h h
-         join t_sale_h a
-              on SUBSTR(a.notes, 0, DECODE(INSTR(a.notes, ' '), 0, LENGTH(a.notes) + 1, INSTR(a.notes, ' ')) - 1) =
-                 h.CFNO
-         join t_sale_d d on a.SALENO = d.SALENO
 where EXISTS(select 1
-from D_LUOSHI_PROG p
-where h.IDCARDNO = p.IDCARDNO
-  and p.PROGRAMME = 1
-  and a.ACCDATE between p.BEGINDATE and p.ENDDATE)
-
+             from D_LUOSHI_PROG p
+             where h.IDCARDNO = p.IDCARDNO
+               and p.PROGRAMME = 1
+               and a.ACCDATE between p.BEGINDATE and p.ENDDATE)
   and d.WAREID IN (10502445);
 
-select BEGINDATE, LEAD(BEGINDATE, 1) OVER (PARTITION BY IDCARDNO ORDER BY BEGINDATE) enddate
-from D_LUOSHI_PROG;
+-- select BEGINDATE, LEAD(BEGINDATE, 1) OVER (PARTITION BY IDCARDNO ORDER BY BEGINDATE) enddate
+-- from D_LUOSHI_PROG;
 
 --方案一
 
@@ -200,23 +187,23 @@ END;
 
 select IDCARDNO, COUNT(*)
 FROM (select IDCARDNO, busno, rn, lastbuytime, 参保地, 异地标志, USERNAME, CAGE, SEX
-from (select h.IDCARDNO, a.busno, cyb.参保地, cyb.异地标志, h.USERNAME, h.CAGE, h.SEX,
-             ROW_NUMBER() over (partition by h.IDCARDNO order by a.ACCDATE desc) rn,
-             MAX(a.ACCDATE) OVER (PARTITION BY h.IDCARDNO,d.WAREID ) AS lastbuytime
-from t_remote_prescription_h h
-         join t_sale_h a on SUBSTR(a.notes, 0,
-                                   DECODE(INSTR(a.notes, ' '), 0, LENGTH(a.notes) + 1,
-                                          INSTR(a.notes, ' ')) -
-                                   1) =
-                            h.CFNO
-         join t_sale_d d on a.SALENO = d.SALENO
-         left join D_ZHYB_HZ_CYB cyb on cyb.ERP销售单号 = a.SALENO
-where
+      from (select h.IDCARDNO, a.busno, cyb.参保地, cyb.异地标志, h.USERNAME, h.CAGE, h.SEX,
+                   ROW_NUMBER() over (partition by h.IDCARDNO order by a.ACCDATE desc) rn,
+                   MAX(a.ACCDATE) OVER (PARTITION BY h.IDCARDNO,d.WAREID ) AS lastbuytime
+            from t_remote_prescription_h h
+                     join t_sale_h a on SUBSTR(a.notes, 0,
+                                               DECODE(INSTR(a.notes, ' '), 0, LENGTH(a.notes) + 1,
+                                                      INSTR(a.notes, ' ')) -
+                                               1) =
+                                        h.CFNO
+                     join t_sale_d d on a.SALENO = d.SALENO
+                     left join D_ZHYB_HZ_CYB cyb on cyb.ERP销售单号 = a.SALENO
+            where
 
 --           a.ACCDATE >= date'2023-01-01' and
 d.WAREID in (10502445, 10601875, 10600308))
-where lastbuytime > date'2023-01-01'
-  and IDCARDNO is not null)
+      where lastbuytime > date'2023-01-01'
+        and IDCARDNO is not null)
 GROUP BY IDCARDNO;
 
 
