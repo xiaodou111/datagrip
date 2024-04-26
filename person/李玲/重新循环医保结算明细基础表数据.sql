d06daace841741df4f6b1d5ea66921548c68eb3f
gege <2575743494@qq.com>
2024-04-26 16:10:31 +0800

    重新循环医保结算明细基础表数据.sql

--- person/李玲/重新循环医保结算明细基础表数据.sql
+++ person/李玲/重新循环医保结算明细基础表数据.sql
@@ -9,28 +9,43 @@ BEGIN
    WHILE P_CurrentDate <= P_EndDate
     LOOP
  insert into D_YBZD_detail
-with a1 as (select yh.RECEIPTDATE,d.BUSNO, tb.CLASSNAME, d.SALENO,d.SALER, d.WAREID, d.WAREQTY, d.NETAMT, d.NETPRICE, ext.WE_SCHAR01,
+with a2 as (select yh.RECEIPTDATE,d.BUSNO, tb.CLASSNAME, d.SALENO,d.SALER, d.WAREID, d.WAREQTY, d.NETAMT, d.NETPRICE,d.ROWNO,
+                   case when yd.EXT_CHAR08=0 then '医保甲' when yd.EXT_CHAR08=1 then '医保丙' else '医保乙' end as WE_SCHAR01,
 --        ext.WE_NUM04 as 药店医保支付价,
 --        ext.WE_NUM05 as 门诊医保支付价,
                    decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05) as 门店诊所医保支付价,
-                   case
+                   --门店诊所医保支付价=0时取售价*
+--                    回返比例<>1时,且医保支付价等于空或0时,去商品单价作为医保支付价
+                   case  when yd.EXT_CHAR08<>1 and nvl(decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05),0)=0 then
+                       d.NETPRICE*(1-yd.EXT_CHAR08)*d.WAREQTY
+                       else
+                   round(LEAST(d.NETPRICE,decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05))*(1-yd.EXT_CHAR08)*d.WAREQTY,4) end as 单据明细医保支付价,
+                  --非医保乙的乙类先自付为0
+                case when yd.EXT_CHAR08  in (1,0) then 0 else
+                    case when nvl(decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05),0)=0 then d.NETPRICE*yd.EXT_CHAR08*d.WAREQTY else
+                   round(LEAST(d.NETPRICE,decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05)),2)*yd.EXT_CHAR08*d.WAREQTY end end as 乙类先自付,
+
+                case
+                    when nvl(decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05),0)=0 then 0
+                        else case
                        when d.NETPRICE > decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05) then
-                           d.NETAMT - d.WAREQTY * decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05)
+                           round(d.NETAMT - d.WAREQTY * decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05),4)
                        else 0
                        --d.NETAMT-d.WAREQTY*d.NETPRICE
-                       end as 医保超限价,
+                       end end as 医保超限价,
                    yd.EXT_CHAR08,
-                   case
-                       when ext.WE_SCHAR01 = '医保乙' then
-                           round(yd.EXT_CHAR08 * (d.NETAMT - case
-                                                                 when d.NETPRICE >
-                                                                      decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05)
-                                                                     then
-                                                                     d.NETAMT - d.WAREQTY *
-                                                                                decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05)
-                                                                 else 0 end), 2)
-                       else 0 end as 乙类先自付,
-                   case when ext.WE_SCHAR01 = '医保丙' then d.NETAMT else 0 end as 医疗费用自费总额,
+--                    case
+--                        --回返比例1=医保丙,0=医保甲,剩余的都是医保乙
+--                        when yd.EXT_CHAR08 not in (0,1) then
+--                            round(yd.EXT_CHAR08 * (d.NETAMT - case
+--                                                                  when d.NETPRICE >
+--                                                                       decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05)
+--                                                                      then
+--                                                                      d.NETAMT - d.WAREQTY *
+--                                                                                 decode(tb.CLASSCODE, '30510', ext.WE_NUM04, '30511', ext.WE_NUM05)
+--                                                                  else 0 end), 2)
+--                        else 0 end as 乙类先自付,
+                   case when yd.EXT_CHAR08=1 then d.NETAMT else 0 end as 医疗费用自费总额,
                    cyb.统筹支付数 as 整单统筹支付数, cyb.个人当年帐户支付数 as 整单个人当年帐户支付数,
                    cyb.个人历年帐户支付数 as 整单个人历年帐户支付数,
                    cyb.公补基金支付数 as 整单公补基金支付数, cyb.大病补助 as 整单大病补助,
@@ -42,40 +57,24 @@ with a1 as (select yh.RECEIPTDATE,d.BUSNO, tb.CLASSNAME, d.SALENO,d.SALER, d.WAR
             from t_sale_d d
                      join D_ZHYB_HZ_CYB cyb on d.SALENO = cyb.ERP销售单号
                      join t_yby_order_h yh on yh.ERPSALENO = cyb.ERP销售单号
-                     join (select ORDERNO,WARECODE,EXT_CHAR08 from T_YBY_ORDER_D_TEMP
+                     join (select ORDERNO,WARECODE,EXT_CHAR08 from T_YBY_ORDER_D
 group by  ORDERNO,WARECODE, EXT_CHAR08) yd  on yh.ORDERNO=yd.ORDERNO and yd.WARECODE=d.WAREID
                      join t_ware_ext ext on d.WAREID = ext.WAREID and ext.COMPID = 1000
                      join t_busno_class_set ts on d.busno = ts.busno and ts.classgroupno = '305'
                      join t_busno_class_base tb on ts.classgroupno = tb.classgroupno and ts.classcode = tb.classcode
                 --and d.SALENO in ('2401304551007053', '2401271247060521')
 --           and cyb.BUSNO in (81499,81501,84576)
-        and  cyb.销售日期 between P_CurrentDate and ADD_MONTHS(P_CurrentDate, 1)
+        and  cyb.销售日期 between trunc(sysdate)-1 and trunc(sysdate)
 --                 and cyb.销售日期>=date'2023-01-01'
         and cyb.异地标志='非异地'
 --   TO_DATE('20240101 07:10:00', 'YYYYMMDD HH24:MI:SS')
 --     AND TO_DATE('20240104 07:10:00', 'YYYYMMDD HH24:MI:SS')
             ),
-     a2 as (select RECEIPTDATE,BUSNO, CLASSNAME, SALENO,SALER, WAREID,MAKENO,INVALIDATE, WAREQTY, NETAMT, NETPRICE, WE_SCHAR01, 门店诊所医保支付价,
-                   医保超限价, EXT_CHAR08,
-                   case
-                       when WE_SCHAR01 = '医保乙' then round(EXT_CHAR08 * (a1.NETAMT - 医保超限价), 2)
-                       else 0 end as 乙类先自付,
-                   医疗费用自费总额, 整单统筹支付数, 整单个人当年帐户支付数, 整单公补基金支付数, 整单大病补助,
-                   整单其他基金支付, 整单家庭共济支付,
-                   整单个人历年帐户支付数, 整单现金支付总额,
-
-                   case
-                       when WE_SCHAR01 = '医保丙' then 0
-                       else NETAMT - case
-                                         when WE_SCHAR01 = '医保乙' then round(EXT_CHAR08 * (a1.NETAMT - 医保超限价), 2)
-                                         else 0 end - 医保超限价
-                       end as 单据明细医保支付价
-            from a1),
-    yb_zfze as (select SALENO, sum(单据明细医保支付价) as 整单医保支付价
+     yb_zfze as (select SALENO, sum(单据明细医保支付价) as 整单医保支付价
                  from a2
                  group by SALENO),
     --每一个每个商品的医保比例
-     yb_bl as (select a2.SALENO, a2.WAREID,a2.WAREQTY,
+     yb_bl as (select a2.SALENO, a2.WAREID,a2.WAREQTY,a2.MAKENO,a2.ROWNO,
                       b.整单医保支付价,
                       case
                                 when b.整单医保支付价 = 0 then 0
@@ -98,11 +97,10 @@ select a2.RECEIPTDATE,a2.BUSNO, a2.CLASSNAME, a2.SALENO,a2.SALER, a2.WAREID,a2.W
        整单家庭共济支付,
        a2.EXT_CHAR08 as 回返比例,
        a2.单据明细医保支付价
-
---
 from a2
          left join yb_zfze b on a2.SALENO = b.SALENO
-         left join yb_bl bl on a2.SALENO = bl.SALENO and a2.WAREID = bl.WAREID  and a2.WAREQTY=bl.WAREQTY;
+         left join yb_bl bl on a2.SALENO = bl.SALENO and a2.WAREID = bl.WAREID  and a2.WAREQTY=bl.WAREQTY and a2.MAKENO=bl.MAKENO
+            and  a2.ROWNO = bl.ROWNO;
   COMMIT;
 
   P_CurrentDate := ADD_MONTHS(P_CurrentDate, 1);
