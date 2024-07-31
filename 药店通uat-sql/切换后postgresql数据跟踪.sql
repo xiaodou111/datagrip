150da2ed8331eac822c53681cb9c413cd901d3e5
gege <2575743494@qq.com>
2024-07-31 19:03:31 +0800

    切换后postgresql数据跟踪.sql

--- 药店通uat-sql/切换后postgresql数据跟踪.sql
+++ 药店通uat-sql/切换后postgresql数据跟踪.sql
@@ -133,6 +133,12 @@ WHERE auth_user_name IS NOT NULL
   AND auth_user_name ~ '^[0-9]+$' -- PostgreSQL 使用 ~ 而不是 REGEXP  !~ '[^0-9]'
   and date(hdr.create_dtme)=CURRENT_DATE
 GROUP BY hdr.sub_unit_num_id,unit.sub_unit_name,auth_user_name,auth_login_name
+---15 零售销售明细
+select hdr.sub_unit_num_id as 门店编码, unit.sub_unit_name as 门店名称, hdr.tml_num_id as 订单号, hdr.create_dtme as 创建时间
+      from sd_bl_so_tml_hdr hdr
+      left join mdms_o_sub_unit unit on hdr.sub_unit_num_id = unit.sub_unit_num_id
+      where hdr.order_date = CURRENT_DATE
+      and hdr.status_num_id = 6;
 
 
 select computer_name as 计算机名, count(*) as 数量
