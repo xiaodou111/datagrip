bf40929510c376d3ac9a218fa71fce2bdf654b1f
gege <2575743494@qq.com>
2024-08-30 15:43:14 +0800

    全亿直连

--- 存储过程/全亿直连总部库存视图存储过程.sql
+++ 存储过程/全亿直连总部库存视图存储过程.sql
@@ -2,6 +2,7 @@ create procedure proc_sjzl_kc_create (
     p_vname in d_rrt_sjzl_config.view_name%type
 )
 is
+  v_accept_view_name d_rrt_sjzl_config.view_name%type;
   v_name d_rrt_sjzl_config.view_name%type;
   v_cnt pls_integer ;
   v_sql varchar2(8000);
@@ -24,9 +25,10 @@ into v_cnt
  end if ;
 
  v_name:=p_vname||'_kc' ;
+ v_accept_view_name:=p_vname||'_accept' ;
 
 ---有的话 根据配置的信息 去生成 视图
-for  res in (SELECT * FROM  d_rrt_sjzl_config  WHERE view_name=p_vname  ) loop   --不想写变量 用循环
+for  res in (SELECT * FROM  d_rrt_sjzl_config  WHERE view_name=p_vname   and view_name not in ('v_sy_ty')  ) loop   --不想写变量 用循环
      --转换一下编码格式
      select f_get_sjzl_rename(res.wareid)
      into v_wareid
@@ -42,22 +44,21 @@ for  res in (SELECT * FROM  d_rrt_sjzl_config  WHERE view_name=p_vname  ) loop  
      into v_werks
      from dual ;
 
- dbms_output.put_line(v_werks);
- dbms_output.put_line(res.qd );
+
 if res.qd is null then
-   v_sql := 'create or replace view '||v_name||' as SELECT * FROM  V_KC_XDL_RT03  WHERE  PHYSIC_QTY>0    and  item_num_id in ('|| v_wareid ||') and CORT_NUM_ID in ('||v_werks||')';
+   v_sql := 'create or replace view '||v_name||' as SELECT * FROM  V_KC_XDL_RT03 a WHERE  PHYSIC_QTY>0    and  item_num_id in ('|| v_wareid ||') and CORT_NUM_ID in ('||v_werks||')';
 
     else
      --拼接sql
-     v_sql :=  'create or replace view '||v_name||' as SELECT * FROM  V_KC_XDL_RT03  WHERE  PHYSIC_QTY>0  and  item_num_id in ('|| v_wareid ||')    and SUPPLY_UNIT_NUM_ID in ( '|| v_qd ||' ) and CORT_NUM_ID in ('||v_werks||')';
+     v_sql :=  'create or replace view '||v_name||' as SELECT * FROM  V_KC_XDL_RT03  a WHERE  PHYSIC_QTY>0  and  item_num_id in ('|| v_wareid ||')   and CORT_NUM_ID in ('||v_werks||') and  exists(select 1 from  ( SELECT ITEM_NUM_ID,batch_id,SUPPLY_UNIT_NUM_ID FROM '||v_accept_view_name ||'  union all  SELECT to_char(wareid),ph,to_char(gysno ) FROM  d_july_kc ) b  where  a.ITEM_NUM_ID=b.ITEM_NUM_ID  and a.BATCH_ID=b.BATCH_ID  and b.SUPPLY_UNIT_NUM_ID in ('||v_qd||' )) '  ;
 
 end if ;
      --拼接导入数据
 v_import:='union all select CORT_NUM_ID 公司编码, CORT_NAME 公司名称, ORDER_DATE 日期, ITEM_NUM_ID 商品编码, ITEM_NAME 商品名称, UNITS_NAME 单位, STYLE_DESC 规格, FACTORY 厂家, BATCH_ID 批号,
-       EXPIRY_DATE 有效期, PHYSIC_QTY 数量, APPROVAL_NO 批准文号, SUPPLY_UNIT_NUM_ID 供应商编码, SUPPLY_NAME 供应商名称
+       EXPIRY_DATE 有效期, PHYSIC_QTY 数量, 0 , APPROVAL_NO 批准文号,SUPPLY_UNIT_NUM_ID,SUPPLY_NAME
 from d_sjzl_kc_dr WHERE REGEXP_LIKE(VIEW_NAME,'''||p_vname||''',''i'')';
 v_sql:=v_sql||v_import;
-     dbms_output.put_line(v_sql);
+--    dbms_output.put_line(v_sql);
       execute immediate  v_sql  ;
 
 
--- 数据直连/全亿配置表字段填充及视图重新生成过程.sql
+++ 数据直连/全亿配置表字段填充及视图重新生成过程.sql
@@ -7,7 +7,7 @@ MERGE INTO d_rrt_sjzl_config T1
 USING
 (
 SELECT factory, LISTAGG(busno, ',') WITHIN GROUP (ORDER BY busno) AS busno_list
-FROM d_sjzl_cjbusno where factory<>'诺华'
+FROM d_sjzl_cjbusno where factory='诺华'
 GROUP BY factory
 )  T2
 ON ( T1.zlcj=T2.factory)
@@ -47,4 +47,100 @@ FROM all_views a
    on  substr(view_name,-4)=unit.sub_unit_num_id
 left join d_rrt_sjzl_config config on config.VIEW_NAME=lower(SUBSTR(a.view_name, 1, INSTR(a.view_name, 'TY') + 1))
 WHERE REGEXP_LIKE(a.view_name, '^v_.+?_ty.*$', 'i')   and SUB_UNIT_NAME not like '%医保%'
+order by config.ZLCJ;
+
+--查找所有厂家的总部视图
+SELECT config.ZLCJ as 厂家名, a.view_name as 视图名称, WERKS as 公司名,
+       case REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1))
+           when 'ACCEPT' THEN '采购'
+           when 'KC' THEN '库存'
+           when 'SALE' THEN '配送'
+           end as 视图类型,
+
+      f_get_sjzl_pjzd(WERKS, 'MDMS_O_CORT', 'CORT_NUM_ID', 'CORT_NAME') as 商业名称,
+      f_get_sjzl_pjzd(WAREID, 'mdms_p_product_basic', 'ITEM_NUM_ID', 'ITEM_NAME') as 商品名称
+FROM all_views a
+         left join qyzt_dev.mdms_o_sub_unit unit
+                   on substr(view_name, -4) = unit.sub_unit_num_id
+         left join d_rrt_sjzl_config config
+                   on config.VIEW_NAME = lower(SUBSTR(a.view_name, 1, INSTR(a.view_name, 'TY') + 1))
+WHERE REGEXP_LIKE(a.view_name, '^v_.+?_ty.*$', 'i') and unit.SUB_UNIT_NAME is null
+  and REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1)) <> '5051'
+--   and config.ZLCJ like '%阿斯利康%'
+order by config.ZLCJ;
+
+
+--查询总部普药视图
+SELECT config.ZLCJ as 厂家名, a.view_name as 视图名称, WERKS as 公司名,
+       case REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1))
+           when 'ACCEPT' THEN '采购'
+           when 'KC' THEN '库存'
+           when 'SALE' THEN '配送'
+           end as 视图类型,
+       f_get_sjzl_pjzd(WERKS, 'MDMS_O_CORT', 'CORT_NUM_ID', 'CORT_NAME') as 商业名称
+--        f_get_sjzl_pjzd(WAREID, 'mdms_p_product_basic', 'ITEM_NUM_ID', 'ITEM_NAME') as 商品名称
+FROM all_views a
+         left join qyzt_dev.mdms_o_sub_unit unit
+                   on substr(view_name, -4) = unit.sub_unit_num_id
+         left join d_rrt_sjzl_config config
+                   on config.VIEW_NAME = lower(SUBSTR(a.view_name, 1, INSTR(a.view_name, 'PY') + 1))
+WHERE REGEXP_LIKE(a.view_name, '^v_.+?_py.*$', 'i') and unit.SUB_UNIT_NAME is null
+  and REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1)) <> '5051'
+  and config.ZLCJ like '%雅培%'
+order by config.ZLCJ;
+--查询总部特药视图
+SELECT config.ZLCJ as 厂家名, a.view_name as 视图名称, WERKS as 公司名,
+       case REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1))
+           when 'ACCEPT' THEN '采购'
+           when 'KC' THEN '库存'
+           when 'SALE' THEN '配送'
+           end as 视图类型,
+       f_get_sjzl_pjzd(WERKS, 'MDMS_O_CORT', 'CORT_NUM_ID', 'CORT_NAME') as 商业名称
+--        f_get_sjzl_pjzd(WAREID, 'mdms_p_product_basic', 'ITEM_NUM_ID', 'ITEM_NAME') as 商品名称
+FROM all_views a
+         left join qyzt_dev.mdms_o_sub_unit unit
+                   on substr(view_name, -4) = unit.sub_unit_num_id
+         left join d_rrt_sjzl_config config
+                   on config.VIEW_NAME = lower(SUBSTR(a.view_name, 1, INSTR(a.view_name, 'TY') + 1))
+WHERE REGEXP_LIKE(a.view_name, '^v_.+?_ty.*$', 'i') and unit.SUB_UNIT_NAME is null
+  and REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1)) <> '5051'
+--   and config.ZLCJ like '%阿斯利康%'
+order by config.ZLCJ;
+
+
+--查询桐乡门店视图
+SELECT config.ZLCJ as 厂家名, a.view_name as 视图名称, substr(a.view_name, -4) as 门店编码,
+       unit.SUB_UNIT_NAME as 门店名称,
+       case
+           when a.view_name like '%ACCEPT%' THEN '采购'
+           when a.view_name like '%KC%' THEN '库存'
+           when a.view_name like '%SALE%' THEN '配送'
+           end as 类型
+FROM all_views a
+         left join qyzt_dev.mdms_o_sub_unit unit
+    -- DDM内部同步工具，字段长度不能超过30位，这个用qyzt_dev
+                   on substr(view_name, -4) = unit.sub_unit_num_id
+         left join d_rrt_sjzl_config config
+                   on config.VIEW_NAME = lower(SUBSTR(a.view_name, 1, INSTR(a.view_name, 'TX') + 1))
+WHERE REGEXP_LIKE(a.view_name, '^v_.+?_tx.*$', 'i') and SUB_UNIT_NAME not like '%医保%'
+  and config.ZLCJ like '%桐乡%'
+order by config.ZLCJ, a.view_name;
+
+--查询桐乡总部视图
+SELECT config.ZLCJ as 厂家名, a.view_name as 视图名称, WERKS as 公司名,
+       case REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1))
+           when 'ACCEPT' THEN '采购'
+           when 'KC' THEN '库存'
+           when 'SALE' THEN '配送'
+           end as 视图类型,
+       f_get_sjzl_pjzd(WERKS, 'MDMS_O_CORT', 'CORT_NUM_ID', 'CORT_NAME') as 商业名称
+--        f_get_sjzl_pjzd(WAREID, 'mdms_p_product_basic', 'ITEM_NUM_ID', 'ITEM_NAME') as 商品名称
+FROM all_views a
+         left join qyzt_dev.mdms_o_sub_unit unit
+                   on substr(view_name, -4) = unit.sub_unit_num_id
+         left join d_rrt_sjzl_config config
+                   on config.VIEW_NAME = lower(SUBSTR(a.view_name, 1, INSTR(a.view_name, 'TX') + 1))
+WHERE REGEXP_LIKE(a.view_name, '^v_.+?_tx.*$', 'i') and unit.SUB_UNIT_NAME is null
+  and REVERSE(SUBSTR(REVERSE(a.view_name), 1, INSTR(REVERSE(a.view_name), '_') - 1)) <> '5051'
+  and config.ZLCJ like '%桐乡%'
 order by config.ZLCJ;
