fe26fc7e602716544fd39c79dccd7496448a9586
gege <2575743494@qq.com>
2024-11-18 16:54:20 +0800

    V_ITEM_INFO.sql

--- view/V_ITEM_INFO.sql
+++ view/V_ITEM_INFO.sql
@@ -1,39 +1,30 @@
-create view V_ITEM_INFO as
-SELECT unit.ITEM_NUM_ID as wareid,unit.CORT_NUM_ID,
- BASIC.ITEM_NAME as WARENAME,BASIC.STYLE_DESC as WARESPEC,BASIC.FACTORY as FACTORYNAME,
- case unit.SUB_PRO_STATUS
-     when 1 then '新品'
-     when 2 then '正常'
-     when 3 then '新品'
-     when 4 then '暂缺'
-     when 5 then '停采'
-     when 6 then '淘汰'
-     when 7 then '清场'
-     when 8 then '停用'
- end as SUB_PRO_STATUS,
-         punit.units_name as WAREUNIT,
- de.POSITIONING_NAME,
- case BASIC.PRESCRIPTION_CLASS
-     when 1 then '甲类OTC'
-     when 2 then '乙类0TC'
-     when 3 then 'DTP'
-     when 4 then '双轨处方'
-     when 5 then '单轨处方'
-     when 6 then '处方'
-end as PRESCRIPTION_CLASS,
-   case  unit.PARTIAL_PRESCRIPT_CLASS
-       when 1 then '甲类OTC'
-       when 2 then '甲类OTC'
-       when 4 then '双轨处方'
-       when 5 then '单轨处方'
-   end as PARTIAL_PRESCRIPT_CLASS,
-    pty1.PTY1_NAME as big,pty2.PTY2_NAME as middle,pty3.PTY3_NAME as SMALL
-FROM mdms_p_product_unit unit
-LEFT JOIN MDMS_P_PRODUCT_BASIC BASIC  ON unit.ITEM_NUM_ID=BASIC.ITEM_NUM_ID
-inner join mdms_p_units punit on basic.tenant_num_id=punit.tenant_num_id and basic.data_sign=punit.data_sign and basic.basic_unit_num_id=punit.units_num_id
-LEFT JOIN mdms_p_catelog_pty1  pty1 on  pty1.PTY1_NUM_ID=BASIC.PTY1_NUM_ID
-LEFT JOIN mdms_p_catelog_pty2  pty2 on  pty2.PTY2_NUM_ID=BASIC.PTY2_NUM_ID
-LEFT JOIN mdms_p_catelog_pty3  pty3 on  pty3.PTY3_NUM_ID=BASIC.PTY3_NUM_ID
-left join (select POSITIONING_NUM_ID,POSITIONING_NAME from mdms_p_positioning group by POSITIONING_NUM_ID, POSITIONING_NAME) de on de.POSITIONING_NUM_ID=unit.sub_pro_description
-/
+create or replace view v_item_info
+as
+SELECT dim_prod_sp_fb.matnr                                  AS wareid,
+       dim_prod_sp_fb.vkorg_id                               AS cort_num_id,
+       dim_prod_sp_fb.psl_pro_name                           AS warename,
+       dim_prod_sp_fb.zgg                                    AS warespec,
+       dim_prod_sp_fb.meins                                  AS wareunit,
+       dim_prod_sp_fb.ztymc                                  AS common_name,
+       dim_prod_sp_fb.zsbpp                                  AS brand_name,
+       dim_prod_sp_fb.zsccj                                  AS factoryname,
+       right(dim_prod_sp_fb.sub_pro_status_notes, 2) AS sub_pro_status,
+       dim_prod_sp_fb.zcffl_nm                               AS partial_prescript_class,
+       dim_prod_sp_fb.zspdw                                  AS positioning_name,
+       dim_prod_sp_fb.dl_name                                AS big,
+       dim_prod_sp_fb.zl_name                                AS middle,
+       dim_prod_sp_fb.xl_name                                AS small,
+       SALE.QTY,
+       SALE.avg_price,
+       SALE.F_AMOUNT,
+       SALE.PRF_AMT,
+       sale.PRF_MLL
+FROM dwb.dim_prod_sp_fb
 
+left join (select vkorg_id,psl_pro_id,sum(psl_qty) as QTY,sum(psl_cb) as PSL_CB,sum(psl_amt) as F_AMOUNT,sum(prf_amt) as SUM_AMT,
+sum(psl_amt)/nullif(sum(psl_qty),0) as avg_price,
+sum(prf_amt)/nullif(sum(psl_qty),0) as PRF_AMT,
+sum(prf_amt)/nullif(sum(psl_cb),0) as PRF_MLL
+from dwb.dwb_sale_basic where  bill_qd_type='O2O' AND psl_date>CURRENT_DATE - interval '90 days'
+and psl_qty>1
+group by  vkorg_id,psl_pro_id) sale on dim_prod_sp_fb.vkorg_id=sale.vkorg_id and dim_prod_sp_fb.matnr=sale.psl_pro_id;
